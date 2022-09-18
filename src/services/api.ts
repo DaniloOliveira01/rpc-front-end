460460fa55e8bb1ee46bc2e86757f62bab0fe4ea
Danilo Oliveira <dan.oliveira883@gmail.com>
2022-09-18 20:26:45 -0300

    :sparkles: feat: add new api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://epg-api.video.globo.com/programmes/'
+  baseURL: 'https://rpc-project-production.up.railway.app/'
 })
