09a75c27f06bde19732d1af48a3faaadb94726d5
Danilo Oliveira <dan.oliveira883@gmail.com>
2022-09-19 10:18:01 -0300

    :sparkles: feat: created pages signin, signup and resetpassword

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,66 @@
-import type { NextPage } from 'next'
-import { Fragment } from 'react'
-import { HomeLayout } from '../components'
+import { NextPage } from "next";
+import { useState } from "react";
+import Image from "next/image";
+import useGet from "../core/hooks/useGet";
+import { useRouter } from "next/router";
 
-const Home: NextPage = () => {
+const Login: NextPage = () => {
+  const { handleSignIn } = useGet();
+  const [email, setEmail] = useState<string>("")
+  const [password, setPassword] = useState<string>("")
+  const router = useRouter()
+  
   return (
-    <Fragment >
-      <HomeLayout />
-    </Fragment>
+    <main className="w-full h-[100vh] flex justify-center items-center">
+      <section className="flex justify-center items-center">
+        <form className="md:p-10 p-5 flex flex-col justify-center items-center gap-5 border-2 rounded-xl" 
+        onSubmit={(e) => handleSignIn(e, email, password)}>
+
+          <div className="relative md:w-[200px] md:h-[160px] w-[150px] h-[95px]">
+            <Image 
+              src={"/img/layout/logo-rpc.webp"} 
+              objectFit={"contain"} 
+              layout={"fill"}
+              priority={true}
+            />
+          </div>
+
+          <input 
+            className="input-global px-3" 
+            placeholder="Email" 
+            type="text" 
+            onChange={(e) => setEmail(e.target.value)} 
+            value={email} 
+          />
+
+          <input 
+            className="input-global px-3" 
+            placeholder="Password" 
+            type="password" 
+            onChange={(e) => setPassword(e.target.value)} 
+            value={password}
+          />
+
+          <button 
+          className="w-[150px] bg-[#6495ED] p-2 text-xl font-semibold">
+            Entrar
+          </button>
+
+          <span
+          onClick={() => router.push("/signup")}
+          className="text-[17px] underline cursor-pointer hover:text-[#1E90FF]">
+            Don't have an account? Register here
+          </span>
+
+          <span
+          onClick={() => router.push("/resetpassword")}
+          className="text-[17px] underline cursor-pointer hover:text-[#1E90FF]">
+            Forgot your password? Click here
+          </span>
+        </form>
+      </section>
+    </main>
   )
 }
 
-export default Home
+export default Login;
