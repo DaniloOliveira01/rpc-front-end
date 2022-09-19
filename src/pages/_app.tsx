1b277e9a8b7736c48636963a6d9db227a240c521
Danilo Oliveira <dan.oliveira883@gmail.com>
2022-09-19 10:18:23 -0300

    :hammer: chore: config toastfy

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
-import "../styles/globals.css";
+import "../core/styles/globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer autoClose={10000} />
+    </>
+  ) 
 }
 
 export default MyApp
